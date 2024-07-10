9b49bf613f78351593ce3fbe852f38f2a73f2b1e
kebsixx <rijikanw@gmail.com>
2024-07-10 10:57:51 +0700

    add to top button

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,17 @@
 // Navbar fixed
 window.onscroll = function () {
   const navbar = document.querySelector("header");
-  navbar.classList.toggle("navbar-fixed", window.scrollY > 0);
+  const toTop = document.querySelector("#toTop");
+
+  if (window.scrollY > 0) {
+    navbar.classList.add("navbar-fixed");
+    toTop.classList.add("flex");
+    toTop.classList.remove("hidden");
+  } else {
+    navbar.classList.remove("navbar-fixed");
+    toTop.classList.remove("flex");
+    toTop.classList.add("hidden");
+  }
 };
 
 // Hamburger
@@ -12,3 +22,10 @@ hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("hamburger-active");
   nav.classList.toggle("hidden");
 });
+
+window.addEventListener("click", function (e) {
+  if (e.target != hamburger && e.target != nav) {
+    hamburger.classList.toggle("hamburger-active");
+    nav.classList.toggle("hidden");
+  }
+})
